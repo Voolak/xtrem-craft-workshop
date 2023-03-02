736e96763f4c888b4960d4ccbfe18c737402a1db
alexandre choplin <achoplin@gmail.com>
2023-03-02 17:50:02 +0100

    Test addition won et USD

--- php/tests/MoneyProblem/PortfolioTest.php
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -14,16 +14,23 @@ class PortfolioTest extends TestCase
 
         // Arrange
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
+        $bank->create(Currency::USD(), Currency::KRW(), 1100);
         $portfolio = new Portfolio;
+        $portfolio2 = new Portfolio;
 
         // Act
         $portfolio->add(5, Currency::USD());
         $portfolio->add(10, Currency::EUR());
 
+        $portfolio2->add(1, Currency::USD());
+        $portfolio2->add(1100, Currency::KRW());
+
         $result = $portfolio->evaluate(Currency::USD(), $bank);
+        $result2 = $portfolio2->evaluate(Currency::KRW(), $bank);
 
         // Assert
-        $this->assertEquals(17,$result);
+        $this->assertEquals(17, $result);
+        $this->assertEquals(2200, $result2);
     }
 
     public function testEmptyPortfolio()
@@ -38,6 +45,6 @@ class PortfolioTest extends TestCase
         $result = $portfolio->evaluate(Currency::USD(), $bank);
 
         // Assert
-        $this->assertEquals(0,$result);
+        $this->assertEquals(0, $result);
     }
 }
